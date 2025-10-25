3098cee728a6d15a2fa0832e137e32251b356de5
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2025-10-24 23:55:50 -0400

    Added Exceptions.Compat

--- src/System/OsString/Internal/Exception/Compat.hs
+++ src/System/OsString/Internal/Exception/Compat.hs
@@ -1,4 +1,7 @@
 module System.OsString.Internal.Exception.Compat
-  (
-
+  ( trySafe
+  , isAsyncException
   ) where
+
+import "os-string" System.OsString.Internal.Exception
+  ( isAsyncException, trySafe )
